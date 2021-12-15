eab5a3f6e2c8a84f2d85a07aaa6fe839d9e7fc52
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2021-12-15 13:48:02 +0200

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,4 +1,5 @@
 from math import *
+from datetime import *
 #def Pindala(külg_1:float, külg_2:float)->float:
 #    """Riistküliku pindala leidmine
 #    :param float külg_1:Riistküliku esimene külg
@@ -48,15 +49,48 @@ from math import *
 #    P=a**4
 #    return S, D, P
 #Task 4
-def season(a:int):
-    """
-    :param int a: Seasons
-    """
-    if a==1 or a==12 or a==2:
-        print("talv")
-    elif a==3 or a==4 or a==5:
-        print("kevad")
-    elif a==6 or a==7 or a==8:
-        print("suvi")
-    elif a==9 or a==10 or a==11:
-        print("sügis")
+#def season(a:int):
+#    """
+#    :param int a: Seasons
+#    """
+#    if a==1 or a==12 or a==2:
+#        print("talv")
+#    elif a==3 or a==4 or a==5:
+#        print("kevad")
+#    elif a==6 or a==7 or a==8:
+#        print("suvi")
+#    elif a==9 or a==10 or a==11:
+#        print("sügis")
+#Task 5
+#def bank(a:float, years:int):
+#    """
+#    :param flaot a: Money
+#    :param int years: Years
+#    """
+#    D=years-2021
+#    while D!=0:
+#        D-=1
+#        a=a/100*10+a
+#        print(float(a))
+#    return a
+#Task 6
+#def is_prime(arv:int):
+#    """
+#    :param int a: arv
+#    """
+#    t=0
+#    if arv<1000 or arv>0:
+#        for i in range(1, arv+1):
+#            if arv%i==0 :
+#                t+=1
+#        if t==2:
+#            t=True
+#        else:
+#            t=False
+#    else:
+#        print("Vale!")
+#    return t
+#Task 7
+def date(d:int, m:int, y:int): 
+    date(y, m, d)
+    return 
